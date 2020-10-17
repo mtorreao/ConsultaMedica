385759cfe662aadbf9c1d098759cd6894bca3be7
Matheus Torreão <mtorreao1@gmail.com>
2020-10-17 16:40:27 -0300

    Atualizando estrutura inicial

---
+++ ConsultaMedica.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsultaMedica.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsultaMedica.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e16ff7e-7842-46f2-9446-1a81d7397441")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConsultaMedica.Transfer/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaMedica.Transfer
+{
+    public class Patient
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime BirthDate { get; set; }
+        public char Sex { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
